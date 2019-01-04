b0a1f17899c38c8f1734f0180778c9f3c9665bba
cvjude <jjchinosoviolet@gmail.com>
2019-01-04 22:58:23 +0100

    Hosting Questioner server on Heroku

---
+++ Server/data/questions.js
@@ -0,0 +1,14 @@
+const questions = [
+	{
+		"id" : "1",
+		"createdOn" : "new Date()",
+		"createdBy" : "Jude", // represents the user asking the question
+		"meetup" : "1", // represents the meetup the question is for
+		"title" : "first question",
+		"body" : "The question is the first one" ,
+		"votes" : "0",
+		"user" : "1"
+	}
+];
+
+export default questions;
