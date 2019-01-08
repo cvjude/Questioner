170becddebed03921c8e58cec0e5b0a33b2ac4e5
cvjude <jjchinosoviolet@gmail.com>
2019-01-08 07:08:51 +0100

    (bug): Fix HTTP status code, change message to error

---
+++ Server/helper/getDataSpec.js
@@ -0,0 +1,47 @@
+import meetups from '../data/meetups';
+
+class GetDataSpec {
+  /**
+  * @Static
+  * @description return the array of all the meetup records according to the given specifications
+  * @memberof questionerController
+  */
+  static getMeetupSpec() {
+    const SpecArray = [];
+    // for (const member of meetups) {
+    meetups.forEach((member) => {
+      const {
+        id, title, location, happeningOn, tags,
+      } = member;
+      const obj = {
+        id, title, location, happeningOn, tags,
+      };
+      SpecArray.push(obj);
+    });
+    return SpecArray;
+  }
+
+  /**
+  * @Static
+  * @description return the array of upcoming meetup records according to the given specifications
+  * @memberof questionerController
+  */
+  static getUpComingSpec() {
+    const SpecArray = [];
+    // for (const member of meetups) {
+    meetups.forEach((member) => {
+      if (member.happeningOn > new Date().toJSON()) {
+        const {
+          id, title, location, happeningOn, tags,
+        } = member;
+        const obj = {
+          id, title, location, happeningOn, tags,
+        };
+        SpecArray.push(obj);
+      }
+    });
+    return SpecArray;
+  }
+}
+
+export default GetDataSpec;
