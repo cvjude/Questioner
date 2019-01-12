bd171f56806968b4a678884cbae50589dd8352e5
cvjude <jjchinosoviolet@gmail.com>
2019-01-12 14:40:32 +0100

    Editing the js files in es6

--- UI/JS/CommentPage.js
+++ UI/JS/CommentPage.js
@@ -1,25 +1,24 @@
 const isAdmin = false;
 
-const button = document.querySelector(".button");
-const form = document.querySelector(".can-hide");
+const button = document.querySelector('.button');
+const form = document.querySelector('.can-hide');
 
-button.addEventListener("click", (event) => {
-	if(form.className === "can-hide"){
-		form.style.display = "block";
-		form.className = "can-view";
-		button.textContent = "Post comment";
-	}
-	else{
-		form.style.display = "none";
-		form.className = "can-hide";
-		button.textContent = "Make comment";
-	}
+button.addEventListener('click', () => {
+  if (form.className === 'can-hide') {
+    form.style.display = 'block';
+    form.className = 'can-view';
+    button.textContent = 'Post comment';
+  } else {
+    form.style.display = 'none';
+    form.className = 'can-hide';
+    button.textContent = 'Make comment';
+  }
 });
 
 const start = () => {
-	if(isAdmin){
-		button.style.display = "none"
-	}
-}
+  if (isAdmin) {
+    button.style.display = 'none';
+  }
+};
 
 onload = start();
--- UI/JS/QuestionPage.js
+++ UI/JS/QuestionPage.js
@@ -1,28 +1,26 @@
-let isAdmin = false;
+const isAdmin = false;
 
-const button = document.querySelectorAll(".button");
-const form = document.querySelector(".can-hide");
-const rsvp = document.querySelector(".rsvp")
+const button = document.querySelectorAll('.button');
+const form = document.querySelector('.can-hide');
+const rsvp = document.querySelector('.rsvp');
 
-button[0].addEventListener("click", (event) => {
-	if(form.className === "can-hide"){
-		form.style.display = "block";
-		form.className = "can-view";
-		button[0].textContent = "Post Question";
-	}
-	else{
-		form.style.display = "none";
-		form.className = "can-hide";
-		button[0].textContent = "Ask Question";
-	}
+button[0].addEventListener('click', () => {
+  if (form.className === 'can-hide') {
+    form.style.display = 'block';
+    form.className = 'can-view';
+    button[0].textContent = 'Post Question';
+  } else {
+    form.style.display = 'none';
+    form.className = 'can-hide';
+    button[0].textContent = 'Ask Question';
+  }
 });
 
 const start = () => {
-	if(isAdmin){
-		button[0].style.display = "none";
-		rsvp.style.display = "none";
-	}
-	console.log("active")
-}
+  if (isAdmin) {
+    button[0].style.display = 'none';
+    rsvp.style.display = 'none';
+  }
+};
 
 onload = start();
